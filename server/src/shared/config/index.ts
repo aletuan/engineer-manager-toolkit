dbfdd111e6d29b50ae649e31e8bed15207614029
Tuan Anh Le <anhlt1983@gmail.com>
2025-03-17 18:24:02 +0700

    fix: export config from shared/config module

--- server/src/shared/config/index.ts
+++ server/src/shared/config/index.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const config = {
+export const config = {
   port: process.env.PORT || 3001,
   nodeEnv: process.env.NODE_ENV || 'development',
   database: {
